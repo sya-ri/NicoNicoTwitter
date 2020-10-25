66f8ed27bc501b3a5bbf87cf4148cf001aa855cf
sya-ri <sya79lua@gmail.com>
2020-10-26 08:35:28 +0900

    プロジェクトを作成

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "me.syari.niconico.twitter"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
