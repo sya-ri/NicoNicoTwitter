807a8151072cc5039b2fb9db172d5c7062cf9f12
sya-ri <sya79lua@gmail.com>
2020-10-26 10:13:13 +0900

    SimpleSearchを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 dependencies {
     implementation("blue.starry:penicillin:5.0.0")
     implementation("io.ktor:ktor-client-cio:1.3.2")
+    implementation("com.soywiz.korlibs.klock:klock-jvm:1.11.3")
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
 }
 
 tasks.withType<KotlinCompile> {
--- src/test/kotlin/me/syari/niconico/twitter/PenicillinTest.kt
+++ src/test/kotlin/me/syari/niconico/twitter/PenicillinTest.kt
@@ -3,10 +3,22 @@ package me.syari.niconico.twitter
 import blue.starry.penicillin.PenicillinClient
 import blue.starry.penicillin.core.session.config.account
 import blue.starry.penicillin.core.session.config.application
-import blue.starry.penicillin.endpoints.oauth
+import blue.starry.penicillin.core.session.config.token
+import blue.starry.penicillin.core.streaming.listener.FilterStreamListener
+import blue.starry.penicillin.endpoints.*
+import blue.starry.penicillin.endpoints.activity.aboutMe
 import blue.starry.penicillin.endpoints.oauth.accessToken
 import blue.starry.penicillin.endpoints.oauth.authenticateUrl
 import blue.starry.penicillin.endpoints.oauth.requestToken
+import blue.starry.penicillin.endpoints.search.search
+import blue.starry.penicillin.endpoints.stream.filter
+import blue.starry.penicillin.endpoints.stream.sample
+import blue.starry.penicillin.endpoints.timeline.userTimeline
+import blue.starry.penicillin.extensions.execute
+import blue.starry.penicillin.extensions.listen
+import blue.starry.penicillin.extensions.queue
+import blue.starry.penicillin.models.Status
+import blue.starry.penicillin.models.Stream
 import kotlinx.coroutines.runBlocking
 
 object PenicillinTest {
@@ -23,6 +35,23 @@ object PenicillinTest {
                 println(client.oauth.authenticateUrl(response.requestToken))
                 val pin = readLine()!!
                 println(client.oauth.accessToken(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY, response.requestToken, response.requestTokenSecret, pin))
+            }
+        }
+    }
+
+    object SimpleSearch {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runBlocking {
+                val client = PenicillinClient {
+                    account {
+                        application(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
+                        token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+                    }
+                }
+                val response = client.search.search("#test").execute()
+                println(response)
+                println(response.json.toString().length)
             }
         }
     }
