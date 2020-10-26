dc6a867bdcb41fb9f61f3846bb32a368c4905a34
sya-ri <sya79lua@gmail.com>
2020-10-26 09:27:09 +0900

    API接続テストを追加 junitではreadLineがハンドルされなかった

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,15 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven(url = "https://dl.bintray.com/nephyproject/stable")
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit"))
+    implementation("blue.starry:penicillin:5.0.0")
+    implementation("io.ktor:ktor-client-cio:1.3.2")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
---
+++ src/test/kotlin/me/syari/niconico/twitter/PenicillinTest.kt
@@ -0,0 +1,29 @@
+package me.syari.niconico.twitter
+
+import blue.starry.penicillin.PenicillinClient
+import blue.starry.penicillin.core.session.config.account
+import blue.starry.penicillin.core.session.config.application
+import blue.starry.penicillin.endpoints.oauth
+import blue.starry.penicillin.endpoints.oauth.accessToken
+import blue.starry.penicillin.endpoints.oauth.authenticateUrl
+import blue.starry.penicillin.endpoints.oauth.requestToken
+import kotlinx.coroutines.runBlocking
+
+object PenicillinTest {
+    object APIConnect {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runBlocking {
+                val client = PenicillinClient {
+                    account {
+                        application(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
+                    }
+                }
+                val response = client.oauth.requestToken()
+                println(client.oauth.authenticateUrl(response.requestToken))
+                val pin = readLine()!!
+                println(client.oauth.accessToken(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY, response.requestToken, response.requestTokenSecret, pin))
+            }
+        }
+    }
+}
