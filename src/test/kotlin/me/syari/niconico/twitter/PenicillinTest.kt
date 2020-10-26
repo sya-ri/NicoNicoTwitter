1d9c6d020bf386d61a76dee49cc9fcf9570aead2
sya-ri <sya79lua@gmail.com>
2020-10-26 11:00:22 +0900

    ContinuousSearchIgnoreRTを追加 継続的なツイート取得に成功

--- src/test/kotlin/me/syari/niconico/twitter/PenicillinTest.kt
+++ src/test/kotlin/me/syari/niconico/twitter/PenicillinTest.kt
@@ -1,6 +1,7 @@
 package me.syari.niconico.twitter
 
 import blue.starry.penicillin.PenicillinClient
+import blue.starry.penicillin.core.session.ApiClient
 import blue.starry.penicillin.core.session.config.account
 import blue.starry.penicillin.core.session.config.application
 import blue.starry.penicillin.core.session.config.token
@@ -11,6 +12,8 @@ import blue.starry.penicillin.endpoints.oauth.requestToken
 import blue.starry.penicillin.endpoints.search
 import blue.starry.penicillin.endpoints.search.search
 import blue.starry.penicillin.extensions.execute
+import blue.starry.penicillin.extensions.models.text
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 object PenicillinTest {
@@ -62,6 +65,34 @@ object PenicillinTest {
                 println(response)
                 println(response.json.toString().length)
             }
+        }
+    }
+
+    object ContinuousSearchIgnoreRT {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runBlocking {
+                val client = PenicillinClient {
+                    account {
+                        application(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
+                        token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+                    }
+                }
+                var sinceId = client.mostRecentId("#test -RT")
+                while (true) {
+                    val response = client.search.search("#test -RT", count = 100, sinceId = sinceId).execute()
+                    sinceId = response.result.searchMetadata.maxId
+                    response.result.statuses.forEach {
+                        println(it.user.screenName + ": " + it.text)
+                    }
+                    delay(5000)
+                }
+            }
+        }
+
+        private suspend fun ApiClient.mostRecentId(query: String): Long? {
+            val response = search.search(query, count = 1).execute()
+            return response.result.searchMetadata.maxId
         }
     }
 }
