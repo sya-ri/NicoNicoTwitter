9f26a5a503c974b7b53746550b8d647152ee06a3
sya-ri <sya79lua@gmail.com>
2020-10-29 11:10:53 +0900

    SwingSimpleAnimationTestを作成。文字列をスライドさせることに成功

---
+++ src/test/kotlin/me/syari/niconico/twitter/SwingSimpleAnimationTest.kt
@@ -0,0 +1,50 @@
+package me.syari.niconico.twitter
+
+import sun.font.FontDesignMetrics
+import java.awt.Graphics
+import java.awt.Rectangle
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.Timer
+
+object SwingSimpleAnimationTest {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        JFrame().apply {
+            defaultCloseOperation = JFrame.EXIT_ON_CLOSE // バツボタンの処理
+            title = "SwingSimpleAnimationTest" // ウィンドウタイトル
+            bounds = Rectangle(900, 600) // ウィンドウサイズを指定
+            setLocationRelativeTo(null) // ウィンドウを中心に配置
+            add(AnimationPanel().apply {
+                start()
+            })
+            isVisible = true // ウィンドウを表示
+        }
+    }
+
+    class AnimationPanel: JPanel() {
+        private val animationTimer = Timer(10) {
+            repaint()
+        }
+
+        fun start() {
+            animationTimer.start()
+        }
+
+        private var helloX = 900
+
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+
+            val fontMetrics = FontDesignMetrics.getMetrics(g.font)
+            val needBounds = fontMetrics.getStringBounds("Hello", null)
+            val isVisible = 0 < (helloX + needBounds.width)
+            if (isVisible) {
+                g.drawString("Hello", helloX, 100)
+                helloX --
+            } else {
+                helloX = 900
+            }
+        }
+    }
+}
