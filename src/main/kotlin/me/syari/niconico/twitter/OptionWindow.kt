d3e8503d6d5fcefd9ac72dbfeb1d1f81fe246ba9
sya-ri <sya79lua@gmail.com>
2020-10-30 10:48:46 +0900

    認証ボタンをクラス化、コメントを追記

--- src/main/kotlin/me/syari/niconico/twitter/OptionWindow.kt
+++ src/main/kotlin/me/syari/niconico/twitter/OptionWindow.kt
@@ -1,7 +1,7 @@
 package me.syari.niconico.twitter
 
 import blue.starry.penicillin.core.exceptions.PenicillinException
-import io.ktor.http.*
+import io.ktor.http.toURI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -9,8 +9,12 @@ import kotlinx.coroutines.withContext
 import me.syari.niconico.twitter.api.TwitterAPI
 import java.awt.Desktop
 import java.awt.Rectangle
-import javax.swing.*
-
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JOptionPane
+import javax.swing.JPanel
+import javax.swing.JTextField
 
 object OptionWindow {
     fun show() {
@@ -21,53 +25,72 @@ object OptionWindow {
             isResizable = false // サイズ変更を無効化
             setLocationRelativeTo(null) // ウィンドウを中心に配置
             add(JPanel().apply {
+                add(JLabel("Twitter"))
                 val twitterIdTextField = add(JTextField(16).apply {
                     isEnabled = false
                 }) as JTextField
-                add(JButton("認証").apply {
-                    addActionListener {
-                        GlobalScope.launch {
-                            isEnabled = false
-                            val generateResult = try {
-                                TwitterAPI.AuthURLProvider.generate()
-                            } catch (ex: PenicillinException) {
-                                JOptionPane("URLの発行に失敗しました").apply {
-                                    createDialog("エラー").apply {
-                                        isAlwaysOnTop = true // ウィンドウを最前面で固定する
-                                        isVisible = true // ウィンドウを表示する
-                                    }
-                                }
-                                return@launch
-                            }
-                            withContext(Dispatchers.IO) {
-                                Desktop.getDesktop().browse(generateResult.url.toURI())
-                            }
-                            val pin = JOptionPane("PINコードを入力してください").apply {
-                                wantsInput = true // 入力を受けつける
-                                createDialog("Twitter 認証").apply {
-                                    isAlwaysOnTop = true // ウィンドウを最前面で固定する
-                                    isVisible = true // ウィンドウを表示する
-                                }
-                            }.inputValue
-                            isEnabled = true
-                            if (pin !is String) return@launch
-                            val accessTokenResponse = try {
-                                TwitterAPI.AuthURLProvider.enterPin(generateResult, pin)
-                            } catch (ex: PenicillinException) {
-                                JOptionPane("認証に失敗しました").apply {
-                                    createDialog("エラー").apply {
-                                        isAlwaysOnTop = true // ウィンドウを最前面で固定する
-                                        isVisible = true // ウィンドウを表示する
-                                    }
-                                }
-                                return@launch
-                            }
-                            twitterIdTextField.text = "@" + accessTokenResponse.screenName
-                        }
-                    }
-                })
+                add(TwitterAuthButton(twitterIdTextField))
             })
             isVisible = true // ウィンドウを表示
+        }
+    }
+
+    class TwitterAuthButton(twitterIdTextField: JTextField): JButton() {
+        init {
+            text = "認証"
+            addActionListener {
+                GlobalScope.launch {
+                    // 多重ウィンドウの防止
+                    isEnabled = false
+
+                    // 認証URLを発行
+                    val generateResult = try {
+                        TwitterAPI.AuthURLProvider.generate()
+                    } catch (ex: PenicillinException) {
+                        JOptionPane("URLの発行に失敗しました").apply {
+                            createDialog("エラー").apply {
+                                isAlwaysOnTop = true // ウィンドウを最前面で固定する
+                                isVisible = true // ウィンドウを表示する
+                            }
+                        }
+                        return@launch
+                    }
+
+                    // 発行したURLを開く
+                    withContext(Dispatchers.IO) {
+                        Desktop.getDesktop().browse(generateResult.url.toURI())
+                    }
+
+                    // PINを入力
+                    val pin = JOptionPane("PINコードを入力してください").apply {
+                        wantsInput = true // 入力を受けつける
+                        createDialog("Twitter 認証").apply {
+                            isAlwaysOnTop = true // ウィンドウを最前面で固定する
+                            isVisible = true // ウィンドウを表示する
+                        }
+                    }.inputValue
+
+                    // 多重ウィンドウの防止
+                    isEnabled = true
+
+                    // 入力したピンで認証
+                    if (pin !is String) return@launch
+                    val accessTokenResponse = try {
+                        TwitterAPI.AuthURLProvider.enterPin(generateResult, pin)
+                    } catch (ex: PenicillinException) {
+                        JOptionPane("認証に失敗しました").apply {
+                            createDialog("エラー").apply {
+                                isAlwaysOnTop = true // ウィンドウを最前面で固定する
+                                isVisible = true // ウィンドウを表示する
+                            }
+                        }
+                        return@launch
+                    }
+
+                    // 認証成功時に TextField の文字列を変更
+                    twitterIdTextField.text = "@" + accessTokenResponse.screenName
+                }
+            }
         }
     }
 }
