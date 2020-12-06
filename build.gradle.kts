6225f43ca2e7baff239760101a70a8c1144b0f29
sya-ri <sya79lua@gmail.com>
2020-12-06 11:23:13 +0900

    依存関係のバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.*
 import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.20"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
@@ -18,8 +18,8 @@ repositories {
 
 dependencies {
     implementation("blue.starry:penicillin:5.0.0")
-    implementation("io.ktor:ktor-client-cio:1.3.2")
-    implementation("com.soywiz.korlibs.klock:klock-jvm:1.11.3")
+    implementation("io.ktor:ktor-client-cio:1.4.1")
+    implementation("com.soywiz.korlibs.klock:klock-jvm:2.0.1")
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
 }
 
